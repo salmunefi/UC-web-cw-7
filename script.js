8a28f475819bc66122e2ac2a1a41a2c697dcc341
Waleed <62436128+WaliiidAls@users.noreply.github.com>
2022-06-12 18:45:43 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+// عرف المصفوفة هنا
+
+function addCourse() {
+  let name = document.getElementById("name").value;
+  let id = document.getElementById("id").value;
+  let phone = document.getElementById("phone").value;
+  let course = document.getElementById("course").value;
+}
+
+function load() {
+  let container = document.getElementById("container");
+}
