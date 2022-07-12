d5ebaacd9a7fcd94e95097b53a794d8a9fb2cbf1
salmunefi <85169532+salmunefi@users.noreply.github.com>
2022-07-12 18:12:50 +0300

    Class work 7

--- script.js
+++ script.js
@@ -1,14 +1,22 @@
 // عرف المصفوفة هنا
+let courses=[]
 
 function addCourse() {
   let name = document.getElementById("name").value;
   let id = document.getElementById("id").value;
   let phone = document.getElementById("phone").value;
   let course = document.getElementById("course").value;
+courses.push=({name,id, phone,course});
+console.log(courses);
   
 }
-
+let container = document.getElementById("container");
 function load() {
-  let container = document.getElementById("container");
-  
+  courses.forEach((course) => {
+  container.innerHTML+=`
+  <div>
+  <h1>${item.name} - ${item.id}</h1>
+  <h3>${item.course}</h3>
+  </div>`
+ });
 }
